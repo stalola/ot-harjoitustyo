4ade2c8a2c8e37c36b49ad1145187ecc350d3ad5
Sanna Talola <48483040+stalola@users.noreply.github.com>
2023-05-13 18:17:34 +0300

    Uudelleenfaktorointi ja buginkorjaus

--- fogapp/src/util/weatherdata.py
+++ fogapp/src/util/weatherdata.py
@@ -14,30 +14,25 @@ class Weatherdata:
         date = datetime.datetime.fromtimestamp(self.timestamp)
         sunrise_utc = self.data["sys"]["sunrise"]
         sunset_utc = self.data["sys"]["sunset"]
-        if name == "date":
-            return date
-        if name == "sunrise_utc":
-            return sunrise_utc
-        if name == "sunset_utc":
-            return sunset_utc
-        return None
-
+        time_values = {"date": date,
+            "sunrise_utc": sunrise_utc,
+            "sunset_utc": sunset_utc
+        }
+        return time_values.get(name, None)
+    
     def get_temperature(self, name):
         temperature_kelv = self.data["main"]["temp"]
         feels_like_kelv = self.data["main"]["feels_like"]
-        temperature_cels = round(temperature_kelv-273.15, 1)
-        feels_like_cels = round(feels_like_kelv-273.15, 1)
-        temperature_fahr = round((temperature_kelv-273.15)*9/2+32, 1)
-        feels_like_fahr = round((feels_like_kelv-273.15)*9/2+32, 1)
-        if name == "temperature_cels":
-            return temperature_cels
-        if name == "temperature_fahr":
-            return temperature_fahr
-        if name == "feels_like_cels":
-            return feels_like_cels
-        if name == "feels_like_fahr":
-            return feels_like_fahr
-        return None
+        temperature_cels = round(temperature_kelv - 273.15, 1)
+        feels_like_cels = round(feels_like_kelv - 273.15, 1)
+        temperature_fahr = round((temperature_kelv - 273.15) * 9 / 5 + 32, 1)
+        feels_like_fahr = round((feels_like_kelv - 273.15) * 9 / 5 + 32, 1)
+        temperature_values = {"temperature_cels": temperature_cels,
+            "temperature_fahr": temperature_fahr,
+            "feels_like_cels": feels_like_cels,
+            "feels_like_fahr": feels_like_fahr}
+        return temperature_values.get(name, None)
+
 
     def get_weather(self, name):
         humidity = self.data["main"]["humidity"]
@@ -46,28 +41,23 @@ class Weatherdata:
         weather_group = self.data["weather"][0]["main"]
         weather_description = self.data["weather"][0]["description"]
         weather_icon = self.data["weather"][0]["icon"]
-        if name == "humidity":
-            return humidity
-        if name == "pressure":
-            return pressure
-        if name == "clouds":
-            return clouds
-        if name == "weather_group":
-            return weather_group
-        if name == "weather_description":
-            return weather_description
-        if name == "weather_icon":
-            return weather_icon
-        return None
+        weather_values = {
+            "humidity": humidity,
+            "pressure": pressure,
+            "clouds": clouds,
+            "weather_group": weather_group,
+            "weather_description": weather_description,
+            "weather_icon": weather_icon
+            }
+        return weather_values.get(name, None)
 
     def get_wind(self, name):
         wind_speed_met = self.data["wind"]["speed"]
         wind_speed_imp = round(wind_speed_met*2.236936, 2)
         wind_deg = self.data["wind"]["deg"]
-        if name == "wind_speed_met":
-            return wind_speed_met
-        if name == "wind_speed_imp":
-            return wind_speed_imp
-        if name == "wind_deg":
-            return wind_deg
-        return None
+        wind_values = {
+            "wind_speed_met": wind_speed_met,
+            "wind_speed_imp": wind_speed_imp,
+            "wind_deg": wind_deg
+        }
+        return wind_values.get(name, None)
